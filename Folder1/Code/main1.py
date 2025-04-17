a85938f4968fb69973592a0a6bd9e23a5ea6e8df
Sandeep U Doddamani <sandeep.doddamani@ecanarys.com>
2025-04-17 11:18:59 +0530

    Update main1.py

--- Folder1/Code/main1.py
+++ Folder1/Code/main1.py
@@ -1,4 +1,10 @@
-from library.lib import shared_util
+import sys
+import os
+
+# Add library folder to Python path
+sys.path.append(os.path.abspath('../library'))
+
+from lib import shared_util
 
 print("Hello from folder1!")
 shared_util()
