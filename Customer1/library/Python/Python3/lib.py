1b5ccf0639dbefc02855f9ec91da567a3d6dbcf6
Sandeep U Doddamani <sandeep.doddamani@ecanarys.com>
2025-04-18 16:03:58 +0530

    Delete Customer1/library/Python/Python3 directory

--- Customer1/library/Python/Python3/lib.py
+++
@@ -1,7 +0,0 @@
-# Shared Python utility
-def shared_util():
-    print("This is a shared Python function.")
-
-
-# update to check commit from Platform-Libs
-# update to check commit from Platform-App
